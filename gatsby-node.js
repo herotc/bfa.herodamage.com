63894629bf63923a7492807260f8753437b24917
Quentin Giraud <dev@aethys.io>
2018-06-03 01:13:22 +0200

    Make nodes corresponding to json reports containing the simulationType and the desired slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,31 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const {createFilePath} = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({node, getNode, boundActionCreators}) => {
+  const {createNodeField} = boundActionCreators
+
+  if (node.sourceInstanceName === 'reports' && node.internal.type === 'File') {
+    // 'Rogue_Trinkets_1T_T21_Subtlety' -> ['rogue', 'trinkets', '1t, 't21', 'subtlety']
+    const nameParts = node.name.toLowerCase().split('_')
+
+    createFilePath({
+      node,
+      getNode,
+      basePath: 'data/reports/'
+    })
+
+    createNodeField({
+      node,
+      name: 'simulationType',
+      value: `${nameParts[1]}` // 'trinkets'
+    })
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/${nameParts[0]}/${nameParts[1]}/${nameParts.slice(2).join('-')}` // '/rogue/trinkets/1t-t21-subtlety'
+    })
+  }
+}
