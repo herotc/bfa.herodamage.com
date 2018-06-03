c1dcd86e454fdfe148432956775d16e823cd5665
Quentin Giraud <dev@aethys.io>
2018-06-03 13:13:04 +0200

    Implements 'build' parameter for reports page, filter only 'reports' files and add all the fields to the GraphQL query

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,32 +14,38 @@ exports.onCreateNode = async ({node, getNode, boundActionCreators}) => {
 
   if (node.sourceInstanceName !== 'reports' || node.internal.type !== 'File') return
 
-  createFilePath({node, getNode, basePath: 'data/reports/'})
+  createFilePath({node, getNode, basePath: 'reports/'})
 
+  // Example file: 'reports/25901/Death-Knight_Trinkets_1T_T21_Frost_Cold-Heart-Runic-Attenuation.json'
   const name = node.name // 'Death-Knight_Trinkets_1T_T21_Frost_Cold-Heart-Runic-Attenuation'
   const nameParts = name.toLowerCase().split('_') // ['death-knight', 'trinkets', '1t', 't21', 'frost', 'cold-heart-runic-attenuation']
-  // slug: '/death-knight/trinkets/1t-t21-frost-cold-heart-runic-attenuation'
-  createNodeField({node, name: 'slug', value: `/${nameParts[0]}/${nameParts[1]}/${nameParts.slice(2).join('-')}`})
+  const [wowClass, simulationType, fightStyle, tier, spec, variation] = nameParts
+  // slug: '/death-knight/25901/trinkets/1t-t21-frost-cold-heart-runic-attenuation'
+  createNodeField({
+    node,
+    name: 'slug',
+    value: `/${wowClass}/${node.relativeDirectory}/${simulationType}/${nameParts.slice(2).join('-')}`
+  })
   // name: 'Death-Knight_Trinkets_1T_T21_Frost_Cold-Heart-Runic-Attenuation'
   createNodeField({node, name: 'name', value: name})
-  // class: 'death-knight'
-  createNodeField({node, name: 'class', value: `${nameParts[0]}`})
+  // wowClass: 'death-knight'
+  createNodeField({node, name: 'wowClass', value: wowClass})
   // simulationType: 'trinkets'
-  createNodeField({node, name: 'simulationType', value: `${nameParts[1]}`})
-  // fightstyle: '1t'
-  createNodeField({node, name: 'fightStyle', value: `${nameParts[2]}`})
+  createNodeField({node, name: 'simulationType', value: simulationType})
+  // fightStyle: '1t'
+  createNodeField({node, name: 'fightStyle', value: fightStyle})
   // tier: 't21'
-  createNodeField({node, name: 'tier', value: `${nameParts[3]}`})
+  createNodeField({node, name: 'tier', value: tier})
   // spec: 'frost'
-  createNodeField({node, name: 'spec', value: `${nameParts[4]}`})
-  // variation: 'cold-heart-runic-attenuation' (optionnal, if it doesn't exist then it's an empty string '')
-  createNodeField({node, name: 'variation', value: `${nameParts[5] || ''}`})
+  createNodeField({node, name: 'spec', value: spec})
+  // variation: 'cold-heart-runic-attenuation' (optional, if it doesn't exist then it's an empty string '')
+  createNodeField({node, name: 'variation', value: variation || ''})
 
   // Get the metas from the file
   const json = JSON.parse(await fs.readFile(node.absolutePath, {encoding: 'utf8', flag: 'r'})).metas
-  const versionUsed = json['options']['dbc']['version_used']
-  createNodeField({node, name: 'version', value: json['options']['dbc'][versionUsed]['wow_version']})
-  createNodeField({node, name: 'build', value: json['options']['dbc'][versionUsed]['build_level']})
+  const versionUsed = json['options']['dbc'][json['options']['dbc']['version_used']]
+  createNodeField({node, name: 'version', value: versionUsed['wow_version']})
+  createNodeField({node, name: 'build', value: versionUsed['build_level']})
   createNodeField({node, name: 'targetError', value: json['options']['target_error']})
   createNodeField({node, name: 'buildTime', value: json['build_timestamp']})
   createNodeField({node, name: 'resultTime', value: json['result_timestamp']})
@@ -50,12 +56,23 @@ exports.createPages = async ({graphql, boundActionCreators}) => {
   const {createPage} = boundActionCreators
   const result = await graphql(`
     {
-      allFile {
+      allFile(filter: {sourceInstanceName: {eq: "reports"}}) {
         edges {
           node {
             fields {
-              simulationType
               slug
+              name
+              wowClass
+              simulationType
+              fightStyle
+              tier
+              spec
+              variation
+              version
+              build
+              targetError
+              buildTime
+              resultTime
             }
           }
         }
