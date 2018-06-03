47aea74cc1364c73a38db774caaedc1279aa414b
Quentin Giraud <dev@aethys.io>
2018-06-03 15:42:49 +0200

    Implements the blog posts markdown sourcing/transforming as a plugin (gatsby-plugin-herodamage-posts)

---
+++ plugins/gatsby-plugin-herodamage-posts/README.md
@@ -0,0 +1,44 @@
+# gatsby-plugin-herodamage-posts
+
+A [Gatsby](https://github.com/gatsbyjs/gatsby) plugin for
+[bfa.herodamage.com](https://github.com/herotc/bfa.herodamage.com).
+Programmatically generate blog posts pages from `src/posts/`.
+
+## Prerequisites
+
+`npm install --save gatsby-source-filesystem gatsby-transformer-remark`
+
+And an entry for `gatsby-transformer-remark` in `gatsby-config.js`:
+```javascript
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: []
+      }
+    }
+  ]
+}
+```
+
+## How to use
+
+Edit `gatsby-config.js`:
+```javascript
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`
+      }
+    },
+    'gatsby-plugin-herodamage-posts'
+  ]
+}
+```
+
+Then implement the `src/templates/blog-posts.js` template file.  
+You can also implements a `src/templates/blog.js` template file to list the posts with a pagination.
---
+++ plugins/gatsby-plugin-herodamage-posts/gatsby-node.js
@@ -0,0 +1,55 @@
+const path = require(`path`)
+const {createFilePath} = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = async ({node, getNode, boundActionCreators}) => {
+  const {createNodeField} = boundActionCreators
+
+  // Prevents non markdown files & non posts files to be processed
+  // Done in two times so we don't have to check if fileAbsolutePath exists since it's a MardownRemark property only
+  if (node.internal.type !== 'MarkdownRemark') return
+  if (node.fileAbsolutePath.indexOf(path.resolve('./src/posts/')) === -1) return
+
+  createFilePath({node, getNode, basePath: 'posts/'})
+
+  // We currently have the MardownRemark node which is a child of the file node that contains markdown information
+  // So we request the parent node, which is the node from filesystem, to build the slug
+  const parentNode = getNode(node.parent)
+  // Example file: 'posts/2018-06-03-test-post.md'
+  const name = parentNode.name // '2018-06-03-test-post'
+  const nameParts = name.toLowerCase().split('-') // ['2018', '06', '03', 'test', 'post']
+  const [year, month, day] = nameParts
+  // slug: '/blog/2018/06/03/test-post'
+  createNodeField({
+    node,
+    name: 'slug',
+    value: `/${year}/${month}/${day}/${nameParts.slice(3).join('-')}`
+  })
+}
+
+exports.createPages = async ({graphql, boundActionCreators}) => {
+  const {createPage} = boundActionCreators
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({node}) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve('./src/templates/blog-posts.js'),
+      context: {
+        slug: node.fields.slug
+      }
+    })
+  })
+}
