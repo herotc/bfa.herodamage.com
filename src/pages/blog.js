231681a316db4b3246fac7a40d23cb0a8d76efcc
Quentin Giraud <dev@aethys.io>
2018-06-03 22:36:30 +0200

    Make the blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const BlogPage = ({data}) => {
+  console.log(data)
+  return (
+    <div>
+      <h1>
+        Blog
+      </h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({node}) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}{' '}
+            <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default BlogPage
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
