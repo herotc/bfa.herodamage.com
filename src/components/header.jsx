4ecde441f5aa61ed690f33a1fe07661d683904c0
Quentin Giraud <dev@aethys.io>
2018-06-04 22:27:40 +0200

    Implements lingui in Layout, Header & Index

---
+++ src/components/header.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+const HeaderElement = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Title = styled.h1`
+  margin: 0;
+`
+
+const SiteLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
+
+const LangSelector = ({lang, onClick}) => (
+  <a onClick={onClick}>{lang}</a>
+)
+LangSelector.propTypes = {
+  lang: PropTypes.string,
+  onClick: PropTypes.func,
+  selected: PropTypes.bool
+}
+const StyledLangSelector = styled(LangSelector)`
+  color: yellow;
+  margin-right: 10px;
+  text-decoration: ${props => props.selected ? 'underline' : 'none'};
+  cursor: pointer;
+`
+
+const Header = ({lang, onLangClick, siteTitle}) => (
+  <HeaderElement>
+    <Container>
+      <Title>
+        <SiteLink to="/">
+          {siteTitle}
+        </SiteLink>
+      </Title>
+      <div>
+        <StyledLangSelector lang='en' onClick={(e) => onLangClick('en')} selected={lang === 'en'}/>
+        <StyledLangSelector lang='fr' onClick={(e) => onLangClick('fr')} selected={lang === 'fr'}/>
+      </div>
+    </Container>
+  </HeaderElement>
+)
+
+Header.propTypes = {
+  lang: PropTypes.string,
+  onLangClick: PropTypes.func,
+  siteTitle: PropTypes.string
+}
+
+export default Header
---
+++ src/layouts/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { I18nProvider } from '@lingui/react'
+import { navigateTo } from 'gatsby-link'
+import Header from '../components/header'
+import { catalogs, prefix, deprefix, langFromPath } from '../../plugins/gatsby-plugin-herodamage-i18n'
+import styled from 'styled-components'
+import './index.css'
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`
+
+const Layout = ({children, data, lang, onLangChange}) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        {name: 'description', content: 'Sample'},
+        {name: 'keywords', content: 'sample, something'}
+      ]}
+    />
+    <Header lang={lang} onLangClick={onLangChange} siteTitle={data.site.siteMetadata.title}/>
+    <Main>{children()}</Main>
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
+  data: PropTypes.object,
+  lang: PropTypes.string,
+  onLangChange: PropTypes.func
+}
+
+class IndexLayout extends React.Component {
+  onLangChange = (lang) => {
+    navigateTo(prefix(lang) + deprefix(this.props.location.pathname))
+  }
+
+  render = () => {
+    const lang = langFromPath(this.props.location.pathname)
+    return (
+      <I18nProvider language={lang} catalogs={catalogs}>
+        <Layout {...this.props} lang={lang} onLangChange={this.onLangChange} />
+      </I18nProvider>
+    )
+  }
+}
+
+IndexLayout.propTypes = {
+  location: PropTypes.object
+}
+
+export default IndexLayout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
