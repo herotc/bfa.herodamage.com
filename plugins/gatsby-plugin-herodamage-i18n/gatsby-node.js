6be4f24a510fb70dc0868676829939de03c51404
Quentin Giraud <dev@aethys.io>
2018-06-05 06:02:00 +0200

    Fix babel preset, no longer pass lang in pathContext, remove 404 pages from i18n blacklist except dev one

--- plugins/gatsby-plugin-herodamage-i18n/gatsby-node.js
+++ plugins/gatsby-plugin-herodamage-i18n/gatsby-node.js
@@ -1,61 +1,40 @@
-// // Add Babel plugin
-// let babelPluginExists = false
-// try {
-//   require.resolve('@lingui/babel-preset-react')
-//   babelPluginExists = true
-// } catch (e) {
-//   // Ignore
-// }
-//
-// exports.modifyBabelrc = ({ babelrc, stage }) => {
-//   if (babelPluginExists) {
-//     if (stage === 'build-html') {
-//       return {
-//         ...babelrc,
-//         plugins: babelrc.plugins.concat([
-//           [
-//             '@lingui/babel-preset-react',
-//             {
-//               ssr: true
-//             }
-//           ]
-//         ])
-//       }
-//     }
-//
-//     return {
-//       ...babelrc,
-//       plugins: babelrc.plugins.concat(['@lingui/babel-preset-react'])
-//     }
-//   }
-//
-//   return babelrc
-// }
+// Add Babel preset
+let babelPresetExists = false
+try {
+  require.resolve('@lingui/babel-preset-react')
+  babelPresetExists = true
+} catch (e) {
+  // Ignore
+}
+
+exports.modifyBabelrc = ({babelrc}) => {
+  if (babelPresetExists) {
+    return {
+      ...babelrc,
+      presets: babelrc.presets.concat(['@lingui/babel-preset-react'])
+    }
+  }
+
+  return babelrc
+}
 
 // Create i18n routes
 const langs = require('./index').langs
 
-exports.onCreatePage = async ({ page, boundActionCreators }) => {
-  const { createPage, deletePage } = boundActionCreators
+exports.onCreatePage = async ({page, boundActionCreators}) => {
+  const {createPage, deletePage} = boundActionCreators
 
-  if (page.path === '/dev-404-page/' || page.path === '/404/' || page.path === '/404.html') return
-  console.log(page.path)
+  if (page.path === '/dev-404-page/') return
 
   // Delete the original page
   deletePage(page)
 
   // Make a new page for each lang
   langs.forEach((lang) => {
-    // Make a new page object from the original page
-    const newPage = Object.assign({}, page)
-
-    // Override the path to add the lang prefix
-    newPage.path = `/${lang}${page.path}`
-
-    // Makes sure we have a context object
-    if (!newPage.context) newPage.context = {}
-    // Then pass it the lang key
-    newPage.context.lang = lang
+    // Make a new page object from the original page and override the path to add the lang prefix
+    const newPage = Object.assign({}, page, {
+      path: `/${lang}${page.path}`
+    })
 
     createPage(newPage)
   })
