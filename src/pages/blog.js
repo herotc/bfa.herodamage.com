e95f429f325c0a7311d456efccfc86427ea58ecf
Quentin Giraud <dev@aethys.io>
2018-08-04 14:13:37 +0200

    do use functional component whenever state isn't involved

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,19 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const BlogPage = ({data}) => {
-  console.log(data)
   return (
     <div>
-      <h1>
-        Blog
-      </h1>
+      <h1>Blog</h1>
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({node}) => (
         <div key={node.id}>
-          <h3>
-            {node.frontmatter.title}{' '}
-            <span>— {node.frontmatter.date}</span>
-          </h3>
+          <h3>{node.frontmatter.title}{' '}<span>— {node.frontmatter.date}</span></h3>
           <p>{node.excerpt}</p>
         </div>
       ))}
@@ -29,7 +23,7 @@ BlogPage.propTypes = {
 export default BlogPage
 
 export const query = graphql`
-  query IndexQuery {
+  query BlogPageIndex {
     allMarkdownRemark {
       totalCount
       edges {
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -3,42 +3,24 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
-class BlogPostTemplate extends React.Component {
-  render () {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const {previous, next} = this.props.pathContext
+const BlogPostTemplate = ({data, pathContext}) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const {previous, next} = pathContext
 
-    return (
-      <div>
-        <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
-        <h1>{post.frontmatter.title}</h1>
-        <p>
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{__html: post.html}}/>
-        <hr/>
-
-        <ul>
-          {previous && (
-            <li>
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            </li>
-          )}
-
-          {next && (
-            <li>
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            </li>
-          )}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
+      <h1>{post.frontmatter.title}</h1>
+      <p>{post.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{__html: post.html}}/>
+      <hr/>
+      <ul>
+        {previous && (<li><Link to={previous.fields.slug} rel="prev">← {previous.frontmatter.title}</Link></li>)}
+        {next && (<li><Link to={next.fields.slug} rel="next">{next.frontmatter.title} →</Link></li>)}
+      </ul>
+    </div>
+  )
 }
 
 BlogPostTemplate.propTypes = {
@@ -49,7 +31,7 @@ BlogPostTemplate.propTypes = {
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
+  query BlogPost($slug: String!) {
     site {
       siteMetadata {
         title
