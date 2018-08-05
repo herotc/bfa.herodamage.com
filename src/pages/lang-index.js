fae8488fffd5d13f305604fe86b7884cb607baa7
Quentin Giraud <dev@aethys.io>
2018-08-06 01:25:10 +0200

    introduction text + fixes

--- src/pages/lang-index.js
+++ src/pages/lang-index.js
@@ -4,14 +4,43 @@ import Link from 'gatsby-link'
 import { Trans, withI18n } from '@lingui/react'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import logo from '../assets/images/logo.svg'
+import { withStyles } from '@material-ui/core/styles/index'
 
-const LangIndexPage = ({i18nPlugin}) => {
+const styles = (theme) => ({
+  section: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    '& figure': {
+      margin: 0,
+      '& img': {
+        height: '10rem',
+        width: '10rem'
+      }
+    }
+  }
+})
+
+const LangIndexPage = ({classes, data, i18nPlugin}) => {
+  const siteMetadata = data.site.siteMetadata
   const {tLink} = i18nPlugin
   return (
     <div>
-      <h1><Trans>Hi people</Trans></h1>
-      <p><Trans>Welcome to your new Gatsby site.</Trans></p>
-      <p><Trans>Now go build something great.</Trans></p>
+      <section className={classes.section}>
+        <figure>
+          <img src={logo} alt={`${data.site.siteMetadata.title} Logo`}/>
+        </figure>
+        <div>
+          <h1 className="site-name">
+            {siteMetadata.title.split(' ').map((titlePart, index) => (<span key={index}>{titlePart}&nbsp;</span>))}
+          </h1>
+          <p><Trans>Welcome to Hero Damage, the website where you can see the latest World of Warcraft simulations
+            results for every class.<br/>
+            Please select your class below.
+          </Trans></p>
+        </div>
+      </section>
       <List>
         <ListItem component={Link} to={tLink('/faq/')}><Trans>Go to faq</Trans></ListItem>
         <ListItem component={Link} to={tLink('/blog/')}><Trans>Go to blog</Trans></ListItem>
@@ -24,7 +53,19 @@ const LangIndexPage = ({i18nPlugin}) => {
 }
 
 LangIndexPage.propTypes = {
+  classes: PropTypes.object,
+  data: PropTypes.object,
   i18nPlugin: PropTypes.object
 }
 
-export default withI18n()(LangIndexPage)
+export default withI18n()(withStyles(styles)(LangIndexPage))
+
+export const query = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
