e4d6c17f1b4645ae2c746588262afa7a8be77fbe
Quentin Giraud <dev@aethys.io>
2018-08-05 17:51:16 +0200

    add siteMetadatas

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Hero Damage'
+    title: 'Hero Damage',
+    url: 'https://bfa.herodamage.com',
+    description: 'Class theorycrafting community, latest simulation results and resources -based on SimulationCraft- for World of Warcraft.',
+    keywords: 'herotc, herodamage, world of warcraft, battle for azeroth, wow bfa, theorycrafting, simulationcraft, simc'
   },
   plugins: [
     'gatsby-plugin-react-next',
