077a3189c66377d9dbb2f109f6f22e65adc0da36
Quentin Giraud <dev@aethys.io>
2018-08-06 20:54:21 +0200

    make sure slug is also translated

--- plugins/gatsby-plugin-herodamage-i18n/gatsby-node.js
+++ plugins/gatsby-plugin-herodamage-i18n/gatsby-node.js
@@ -39,12 +39,17 @@ module.exports.onCreatePage = function ({page, boundActionCreators}) {
 
   // TODO: Support pages made with 'name.lang.ext' like 'fancy-post.fr.md'
 
+  // Update the slug if there is any to reflect what the path is
+  const pageContext = page.context
+  const {slug} = pageContext
+
   // Make a new page for each lang
   langs.forEach((lang) => {
     // Object.assign is used to avoid mutating the page object
-    const context = {context: Object.assign({}, page.context, {lang})}
-    const path = {path: `/${lang}${pagePath}`}
-    const newPage = Object.assign({}, page, path, context)
+    const context = Object.assign({}, pageContext, {lang})
+    if (slug) context.slug = `/${lang}${slug}`
+    const path = `/${lang}${pagePath}`
+    const newPage = Object.assign({}, page, {path}, {context})
     createPage(newPage)
   })
 }
