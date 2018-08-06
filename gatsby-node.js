2f867abe0ae7d45b5dc422e84b2080988dc6687e
Quentin Giraud <dev@aethys.io>
2018-08-06 22:14:42 +0200

    fix prod build with async function

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.modifyBabelrc = ({babelrc}) => ({
+  ...babelrc,
+  plugins: babelrc.plugins.concat(
+    ['transform-regenerator'],
+    ['transform-runtime']
+  )
+})
