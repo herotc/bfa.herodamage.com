ce61b3d55862c5c9959e61ae5e9014846bd4710d
Quentin Giraud <dev@aethys.io>
2018-08-07 01:07:24 +0200

    fighstyles bar + cleanup & fixes

--- src/templates/simulation/races.js
+++ src/templates/simulation/races.js
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Trans, withI18n } from '@lingui/react'
 import { withStyles } from '@material-ui/core/styles/index'
+import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { racesInit } from '../../browser/charts/races'
+import Link from 'gatsby-link/index'
 
 const styles = (theme) => ({
 
@@ -11,9 +13,9 @@ const styles = (theme) => ({
 
 class RacesSimulationTemplate extends React.Component {
   componentDidMount () {
-    const {data} = this.props
+    const {data, pathContext} = this.props
     const {reportsPath} = data.site.siteMetadata
-    const {name, templateDPS} = data.sitePage.context
+    const {name, templateDPS} = pathContext
     try {
       racesInit(`${reportsPath}${name}`, 'Test', templateDPS)
     } catch (err) {
@@ -22,9 +24,8 @@ class RacesSimulationTemplate extends React.Component {
   }
 
   render () {
-    const {data, i18nPlugin} = this.props
-    const {t} = i18nPlugin
-    const {name} = data.sitePage.context
+    const {data, pathContext} = this.props
+    const {name, fightStyle} = pathContext
     return (
       <div>
         <h1>{name.replace(new RegExp('_', 'g'), ' ').replace(new RegExp('-', 'g'), ' ')}</h1>
@@ -33,6 +34,20 @@ class RacesSimulationTemplate extends React.Component {
           out the following chart. However, keep in mind that race differences are usually rather small and can change
           with balance and gameplay changes. You can always play the race you want to play and don&apos,t have to feel
           compelled to choose based on this chart.</Trans></p>
+        <div style={{textAlign: 'center'}}>
+          {
+            data.allSitePage.edges.map((edge, index) => {
+              const {node} = edge
+              const {fightStyle: nodeFightStyle} = node.context
+              return (
+                <Button key={index} variant="contained" color="primary" disabled={fightStyle === nodeFightStyle}
+                  component={Link} to={node.path} style={{margin: 8}}>
+                  {nodeFightStyle}
+                </Button>
+              )
+            })
+          }
+        </div>
         <CircularProgress id="results-loader" color="secondary"/>
         <div id="chart-overlay"/>
         <div id="google-chart" style={{height: 500, width: '100%'}}/>
@@ -44,32 +59,26 @@ class RacesSimulationTemplate extends React.Component {
 RacesSimulationTemplate.propTypes = {
   classes: PropTypes.object,
   data: PropTypes.object,
-  i18nPlugin: PropTypes.object
+  pathContext: PropTypes.object
 }
 
 export default withI18n()(withStyles(styles)(RacesSimulationTemplate))
 
 export const query = graphql`
-  query RacesSimulation($slugIntl: String!) {
+  query RacesSimulation($lang: String!, $spec: String!, $simulationType: String!, $tier: String!) {
     site {
       siteMetadata {
         reportsPath
       }
     }
-    sitePage(path: {eq: $slugIntl}) {
-      context {
-        name
-        fightStyle
-        tier
-        spec
-        variation
-        targetError
-        resultTime
-        version
-        build
-        buildTime
-        gitRevision
-        templateDPS
+    allSitePage(filter: {context: {lang: {eq: $lang}, spec: {eq: $spec}, simulationType: {eq: $simulationType}, tier: {eq: $tier}}}) {
+      edges {
+        node {
+          path
+          context {
+            fightStyle
+          }
+        }
       }
     }
   }
--- src/templates/simulation/trinkets.js
+++ src/templates/simulation/trinkets.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
 import { Trans, withI18n } from '@lingui/react'
 import { withStyles } from '@material-ui/core/styles/index'
+import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { trinketsInit } from '../../browser/charts/trinkets'
 
@@ -9,9 +11,9 @@ const styles = (theme) => ({})
 
 class TrinketsSimulationTemplate extends React.Component {
   componentDidMount () {
-    const {data} = this.props
+    const {data, pathContext} = this.props
     const {reportsPath} = data.site.siteMetadata
-    const {name, templateDPS} = data.sitePage.context
+    const {name, templateDPS} = pathContext
     try {
       trinketsInit(`${reportsPath}${name}`, 'Test', templateDPS)
     } catch (err) {
@@ -20,9 +22,8 @@ class TrinketsSimulationTemplate extends React.Component {
   }
 
   render () {
-    const {data, i18nPlugin} = this.props
-    const {t} = i18nPlugin
-    const {name} = data.sitePage.context
+    const {data, pathContext} = this.props
+    const {name, fightStyle} = pathContext
     return (
       <div>
         <h1>{name.replace(new RegExp('_', 'g'), ' ').replace(new RegExp('-', 'g'), ' ')}</h1>
@@ -32,6 +33,20 @@ class TrinketsSimulationTemplate extends React.Component {
           simulations are based on predefined gear sets instead of your own, after all. This means data shown here <b>depends
             heavily</b> on the used profile with its talents, legendaries etc. and is rather giving an outlook. If your
           character is different from the setup used here, personal simulations are recommended.</Trans></p>
+        <div style={{textAlign: 'center'}}>
+          {
+            data.allSitePage.edges.map((edge, index) => {
+              const {node} = edge
+              const {fightStyle: nodeFightStyle} = node.context
+              return (
+                <Button key={index} variant="contained" color="primary" disabled={fightStyle === nodeFightStyle}
+                  component={Link} to={node.path} style={{margin: 8}}>
+                  {nodeFightStyle}
+                </Button>
+              )
+            })
+          }
+        </div>
         <CircularProgress id="results-loader" color="secondary"/>
         <div id="chart-overlay"/>
         <div id="google-chart" style={{height: 500, width: '100%'}}/>
@@ -43,34 +58,27 @@ class TrinketsSimulationTemplate extends React.Component {
 TrinketsSimulationTemplate.propTypes = {
   classes: PropTypes.object,
   data: PropTypes.object,
-  i18nPlugin: PropTypes.object
+  pathContext: PropTypes.object
 }
 
 export default withI18n()(withStyles(styles)(TrinketsSimulationTemplate))
 
 export const query = graphql`
-  query TrinketsSimulation($slugIntl: String!) {
+  query TrinketsSimulation($lang: String!, $spec: String!, $simulationType: String!, $tier: String!) {
     site {
       siteMetadata {
         reportsPath
       }
     }
-    sitePage(path: {eq: $slugIntl}) {
-      context {
-        name
-        fightStyle
-        tier
-        spec
-        variation
-        targetError
-        resultTime
-        version
-        build
-        buildTime
-        gitRevision
-        templateDPS
+    allSitePage(filter: {context: {lang: {eq: $lang}, spec: {eq: $spec}, simulationType: {eq: $simulationType}, tier: {eq: $tier}}}) {
+      edges {
+        node {
+          path
+          context {
+            fightStyle
+          }
+        }
       }
     }
   }
 `
-
