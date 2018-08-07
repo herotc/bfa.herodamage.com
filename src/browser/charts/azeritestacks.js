87977e3c645e15808b58ffd583cf9371847779e2
Quentin Giraud <dev@aethys.io>
2018-08-07 20:27:15 +0200

    azerite stacks charts

---
+++ src/browser/charts/azeritestacks.js
@@ -0,0 +1,128 @@
+import load from 'little-loader'
+import { formatNumber, excludeEmptyRows, removeLoading, initOverlay } from './common'
+
+export async function azeriteStacksInit (reportPath, chartTitle, templateDPS) {
+  if (!window.google) {
+    await new Promise((resolve, reject) => {
+      load('https://www.gstatic.com/charts/loader.js', (err) => {
+        if (err) reject(err)
+        resolve()
+      })
+    })
+  }
+
+  const google = window.google
+
+  const drawChart = async () => {
+    const response = await window.fetch(`${reportPath}.json`)
+    const json = await response.json()
+    const data = new google.visualization.arrayToDataTable(json.results)
+
+    // Sorting
+    const sortCol = data.addColumn('number')
+    for (let row = 0; row < data.getNumberOfRows(); row++) {
+      let biggestTotalValue = 0
+      for (let col = 1; col < sortCol; col++) {
+        if (data.getValue(row, col) > biggestTotalValue) { biggestTotalValue = data.getValue(row, col) }
+      }
+      data.setValue(row, sortCol, biggestTotalValue)
+    }
+    data.sort([{column: sortCol, desc: true}])
+    data.removeColumn(sortCol)
+
+    // Add Tooltip columns
+    for (let col = 2; col <= data.getNumberOfColumns(); col += 2) {
+      data.insertColumn(col, {type: 'string', role: 'tooltip', 'p': {'html': true}})
+    }
+
+    // Calculate Differences
+    for (let row = 0; row < data.getNumberOfRows(); row++) {
+      let prevVal = 0
+      let prevAbsVal = 0
+      for (let col = 1; col < data.getNumberOfColumns(); col += 2) {
+        const curAbsVal = data.getValue(row, col)
+        const absStepVal = curAbsVal - prevAbsVal
+        const curVal = 100 * ((templateDPS + curAbsVal) / templateDPS - 1)
+        const stepVal = curVal - prevVal
+        const tooltip = `
+        <div class="chart-tooltip">
+          <b>${data.getValue(row, 0)}<br/> Item Level ${data.getColumnLabel(col)}</b><br/>
+          <b>Total:</b> ${formatNumber(curVal.toFixed(2))} % (${formatNumber(curAbsVal.toFixed())})<br/>
+          <b>Increase:</b> ${formatNumber(stepVal.toFixed(2))}% (${formatNumber(absStepVal.toFixed())} )
+        </div>`
+        data.setValue(row, col + 1, tooltip)
+        data.setValue(row, col, stepVal)
+        prevVal = curVal > prevVal ? curVal : prevVal
+        prevAbsVal = curAbsVal > prevAbsVal ? curAbsVal : prevAbsVal
+      }
+    }
+
+    // Get content width (to force a min-width on mobile, can't do it in css because of the overflow)
+    const googleChartElement = document.getElementById('google-chart')
+    const content = googleChartElement.parentElement
+    const contentWidth = content.innerWidth - window.getComputedStyle(content, null).getPropertyValue('padding-left') * 2
+
+    // Set chart options
+    const chartWidth = document.documentElement.clientWidth >= 768 ? contentWidth : 700
+    const bgColor = '#303030'
+    const textColor = '#ffffff'
+    const options = {
+      title: chartTitle,
+      backgroundColor: bgColor,
+      chartArea: {
+        top: 50,
+        bottom: 100,
+        right: 150,
+        left: 200
+      },
+      hAxis: {
+        gridlines: {
+          count: 20
+        },
+        format: '#.#\'%\'',
+        textStyle: {
+          color: textColor
+        },
+        title: '% DPS Gain',
+        titleTextStyle: {
+          color: textColor
+        },
+        viewWindowMode: 'maximized',
+        viewWindow: {
+          min: 0
+        }
+      },
+      vAxis: {
+        textStyle: {
+          fontSize: 12,
+          color: textColor
+        },
+        titleTextStyle: {
+          color: textColor
+        }
+      },
+      legend: {
+        textStyle: {
+          color: textColor
+        }
+      },
+      titleTextStyle: {
+        color: textColor
+      },
+      tooltip: {
+        isHtml: true
+      },
+      isStacked: true,
+      width: chartWidth
+    }
+
+    // Instantiate and draw our chart, passing in some options.
+    const chart = new google.visualization.BarChart(googleChartElement)
+    chart.draw(excludeEmptyRows(data), options)
+    removeLoading()
+    initOverlay(options.chartArea)
+  }
+
+  google.charts.load('current', {'packages': ['corechart']})
+  google.charts.setOnLoadCallback(drawChart)
+}
--- src/templates/simulation/azerite-stacks.js
+++ src/templates/simulation/azerite-stacks.js
@@ -1,7 +1,82 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import { Trans, withI18n } from '@lingui/react'
+import { withStyles } from '@material-ui/core/styles/index'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { azeriteLevelsInit } from '../../browser/charts/azeritelevels'
 
-const AzeriteStacksSimulationTemplate = () => (
-  <h1>Azerite Stack simulations</h1>
-)
+const styles = (theme) => ({})
 
-export default AzeriteStacksSimulationTemplate
+class AzeriteStacksSimulationTemplate extends React.Component {
+  componentDidMount () {
+    const {data, pathContext} = this.props
+    const {reportsPath} = data.site.siteMetadata
+    const {name, templateDPS} = pathContext
+    azeriteLevelsInit(`${reportsPath}${name}`, 'Test', templateDPS)
+      .catch((err) => { console.error(err) })
+  }
+
+  render () {
+    const {data, pathContext} = this.props
+    const {name, fightStyle} = pathContext
+    return (
+      <div>
+        <h1>{name.replace(new RegExp('_', 'g'), ' ').replace(new RegExp('-', 'g'), ' ')}</h1>
+        <p><Trans>Here, you can compare expected DPS increase from azerite powers.</Trans></p>
+        <p><Trans>In order to compare azerite powers with this chart, look for the end of the bars corresponding to the
+          item
+          level of interest. However, you should <b>simulate your own character</b> to find your best setup. These
+          simulations are based on predefined gear sets instead of your own, after all. This means data shown here <b>depends
+            heavily</b> on the used profile with its talents, its gear, etc. and is rather giving an outlook. If your
+          character is different from the setup used here, personal simulations are recommended.</Trans></p>
+        <div style={{textAlign: 'center'}}>
+          {
+            data.allSitePage.edges.map((edge, index) => {
+              const {node} = edge
+              const {fightStyle: nodeFightStyle} = node.context
+              return (
+                <Button key={index} variant="contained" color="primary" disabled={fightStyle === nodeFightStyle}
+                  component={Link} to={node.path} style={{margin: 8}}>
+                  {nodeFightStyle}
+                </Button>
+              )
+            })
+          }
+        </div>
+        <CircularProgress id="results-loader" color="secondary"/>
+        <div id="chart-overlay"/>
+        <div id="google-chart" style={{height: 1200, width: '100%'}}/>
+      </div>
+    )
+  }
+}
+
+AzeriteStacksSimulationTemplate.propTypes = {
+  classes: PropTypes.object,
+  data: PropTypes.object,
+  pathContext: PropTypes.object
+}
+
+export default withI18n()(withStyles(styles)(AzeriteStacksSimulationTemplate))
+
+export const query = graphql`
+  query AzeriteStacksSimulation($lang: String!, $spec: String!, $simulationType: String!, $tier: String!, $variation: String!) {
+    site {
+      siteMetadata {
+        reportsPath
+      }
+    }
+    allSitePage(filter: {context: {lang: {eq: $lang}, spec: {eq: $spec}, simulationType: {eq: $simulationType}, tier: {eq: $tier}, variation: {eq: $variation}}}) {
+      edges {
+        node {
+          path
+          context {
+            fightStyle
+          }
+        }
+      }
+    }
+  }
+`
