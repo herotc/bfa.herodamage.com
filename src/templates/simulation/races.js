654b8687d76b5e28b48750f2da4887c834f9ee32
Quentin Giraud <dev@aethys.io>
2018-08-09 18:50:51 +0200

    major refactor of charts template

--- src/templates/simulation/races.js
+++ src/templates/simulation/races.js
@@ -1,69 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
 import { Trans } from '@lingui/react'
-import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import { racesInit } from '../../browser/charts/races'
-import GoogleAd from '../../components/google-ad'
+import StackedChartLayout from './common/stacked-chart-layout'
 
-class RacesSimulationTemplate extends React.Component {
-  componentDidMount () {
-    const {data, pathContext} = this.props
-    const {reportsPath} = data.site.siteMetadata
-    const {name, templateDPS} = pathContext
-    try {
-      racesInit(`${reportsPath}${name}.json`, 'Race % DPS Gain', templateDPS)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  render () {
-    const {data, i18nPlugin, location, pathContext} = this.props
-    const {reportsPath} = data.site.siteMetadata
-    const {t} = i18nPlugin
-    const {name, fightStyle} = pathContext
-    return (
-      <div>
-        <Helmet>
-          <link rel="prefetch" href={`${reportsPath}${name}.json`}/>
-        </Helmet>
-        <h1>{name.replace(new RegExp('_', 'g'), ' ').replace(new RegExp('-', 'g'), ' ')}</h1>
-        <p><Trans>If you are interested in how the different races in World of Warcraft compare for this build, you can
-          check out the following chart. However, keep in mind that race differences are usually rather small and can
-          change with balance and gameplay changes. You can always play the race you want to play and don&apos,t have
-          to feel compelled to choose based on this chart.</Trans></p>
-        <div style={{textAlign: 'center'}}>
-          {
-            data.allSitePage.edges.map((edge, index) => {
-              const {node} = edge
-              const {fightStyle: nodeFightStyle} = node.context
-              return (
-                <Button key={index} variant="contained" color="primary" disabled={fightStyle === nodeFightStyle}
-                  component={Link} to={node.path} style={{margin: 8}}>
-                  {t(nodeFightStyle)}
-                </Button>
-              )
-            })
-          }
-        </div>
-        <GoogleAd location={location} type="inarticle"/>
-        <CircularProgress id="results-loader" color="secondary"/>
-        <div id="chart-overlay"/>
-        <div id="google-chart" style={{height: 500, width: '100%'}}/>
-      </div>
-    )
-  }
-}
-
-RacesSimulationTemplate.propTypes = {
-  data: PropTypes.object,
-  i18nPlugin: PropTypes.object,
-  location: PropTypes.object,
-  pathContext: PropTypes.object
-}
+const RacesSimulationTemplate = (props) => (
+  <StackedChartLayout {...props} chartTitle="Race % DPS Gain">
+    <div>
+      <p><Trans>If you are interested in how the different races in World of Warcraft compare for this build, you can
+        check out the following chart. However, keep in mind that race differences are usually rather small and can
+        change with balance and gameplay changes. You can always play the race you want to play and don&apos,t have
+        to feel compelled to choose based on this chart.</Trans></p>
+    </div>
+  </StackedChartLayout>
+)
 
 export default RacesSimulationTemplate
 
--- src/templates/simulation/trinkets.js
+++ src/templates/simulation/trinkets.js
@@ -1,71 +1,19 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
 import { Trans } from '@lingui/react'
-import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import { trinketsInit } from '../../browser/charts/trinkets'
-import GoogleAd from '../../components/google-ad'
+import StackedChartLayout from './common/stacked-chart-layout'
 
-class TrinketsSimulationTemplate extends React.Component {
-  componentDidMount () {
-    const {data, pathContext} = this.props
-    const {reportsPath} = data.site.siteMetadata
-    const {name, templateDPS} = pathContext
-    try {
-      trinketsInit(`${reportsPath}${name}.json`, 'Trinkets % DPS Gain per Item Levels', templateDPS)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  render () {
-    const {data, i18nPlugin, location, pathContext} = this.props
-    const {reportsPath} = data.site.siteMetadata
-    const {t} = i18nPlugin
-    const {name, fightStyle} = pathContext
-    return (
-      <div>
-        <Helmet>
-          <link rel="prefetch" href={`${reportsPath}${name}.json`}/>
-        </Helmet>
-        <h1>{name.replace(new RegExp('_', 'g'), ' ').replace(new RegExp('-', 'g'), ' ')}</h1>
-        <p><Trans>Here, you can compare expected DPS increase from trinkets.</Trans></p>
-        <p><Trans>In order to compare trinkets with this chart, look for the end of the bars corresponding to the item
-          level of interest. However, you should <b>simulate your own character</b> to find your best setup. These
-          simulations are based on predefined gear sets instead of your own, after all. This means data shown here <b>depends
-            heavily</b> on the used profile with its talents, its gear, etc. and is rather giving an outlook. If your
-          character is different from the setup used here, personal simulations are recommended.</Trans></p>
-        <div style={{textAlign: 'center'}}>
-          {
-            data.allSitePage.edges.map((edge, index) => {
-              const {node} = edge
-              const {fightStyle: nodeFightStyle} = node.context
-              return (
-                <Button key={index} variant="contained" color="primary" disabled={fightStyle === nodeFightStyle}
-                  component={Link} to={node.path} style={{margin: 8}}>
-                  {t(nodeFightStyle)}
-                </Button>
-              )
-            })
-          }
-        </div>
-        <GoogleAd location={location} type="inarticle"/>
-        <CircularProgress id="results-loader" color="secondary"/>
-        <div id="chart-overlay"/>
-        <div id="google-chart" style={{height: 750, width: '100%'}}/>
-      </div>
-    )
-  }
-}
-
-TrinketsSimulationTemplate.propTypes = {
-  data: PropTypes.object,
-  i18nPlugin: PropTypes.object,
-  location: PropTypes.object,
-  pathContext: PropTypes.object
-}
+const TrinketsSimulationTemplate = (props) => (
+  <StackedChartLayout {...props} chartTitle="Race % DPS Gain">
+    <div>
+      <p><Trans>Here, you can compare expected DPS increase from trinkets.</Trans></p>
+      <p><Trans>In order to compare trinkets with this chart, look for the end of the bars corresponding to the item
+        level of interest. However, you should <b>simulate your own character</b> to find your best setup. These
+        simulations are based on predefined gear sets instead of your own, after all. This means data shown here <b>depends
+          heavily</b> on the used profile with its talents, its gear, etc. and is rather giving an outlook. If your
+        character is different from the setup used here, personal simulations are recommended.</Trans></p>
+    </div>
+  </StackedChartLayout>
+)
 
 export default TrinketsSimulationTemplate
 
