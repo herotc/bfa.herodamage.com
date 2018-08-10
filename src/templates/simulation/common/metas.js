7a73487e2f9fbfaf68eb55aa59b13f2a0d1e3533
Quentin Giraud <dev@aethys.io>
2018-08-10 12:37:17 +0200

    implements Metas in stacked chart template

---
+++ src/templates/simulation/common/metas.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Trans, DateFormat } from '@lingui/react'
+
+const Metas = ({buildTime, gitRevision, targetError, version}) => {
+  const buildDate = new Date(buildTime * 1000)
+  return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <p>
+        <Trans><b>Last Update:</b></Trans>&nbsp;
+        <DateFormat value={buildDate}
+          format={{month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit'}}/> (CEST)<br/>
+        <Trans><b>WoW-Build:</b></Trans> {version}
+        <a href={`https://github.com/simulationcraft/simc/commit/${gitRevision}`} title="Corresponding SimC commit"
+          target="_blank" rel="noopener noreferrer nofollow">#{gitRevision}</a><br/>
+        <Trans><b>Target Error:</b></Trans> {targetError}%
+      </p>
+    </div>
+  )
+}
+
+Metas.propTypes = {
+  buildTime: PropTypes.string.isRequired,
+  gitRevision: PropTypes.string.isRequired,
+  targetError: PropTypes.string.isRequired,
+  version: PropTypes.string.isRequired
+}
+
+export default Metas
