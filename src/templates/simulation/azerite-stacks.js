3983ccbb71fb23cbbe0497be91e6bfd8b0408da8
Quentin Giraud <dev@aethys.io>
2018-08-19 00:09:28 +0200

    some text updates

--- src/templates/simulation/azerite-stacks.js
+++ src/templates/simulation/azerite-stacks.js
@@ -9,8 +9,7 @@ const AzeriteStacksSimulationTemplate = (props) => (
     <div>
       <p><Trans>Here, you can compare expected DPS increase from azerite powers.</Trans></p>
       <p><Trans>In order to compare azerite powers with this chart, look for the end of the bars corresponding to the
-        item
-        level of interest. However, you should <b>simulate your own character</b> to find your best setup. These
+        item level of interest. However, you should <b>simulate your own character</b> to find your best setup. These
         simulations are based on predefined gear sets instead of your own, after all. This means data shown here <b>depends
           heavily</b> on the used profile with its talents, its gear, etc. and is rather giving an outlook. If your
         character is different from the setup used here, personal simulations are recommended.</Trans></p>
