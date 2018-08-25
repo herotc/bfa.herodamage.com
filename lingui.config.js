75790ede374e86da997b4b7a3f7f26bacc149419
Quentin Giraud <dev@aethys.io>
2018-08-26 00:19:46 +0200

    move i18n cli into scripts/i18n

---
+++ lingui.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  fallbackLocale: 'en',
+  sourceLocale: 'en',
+  localeDir: '<rootDir>/src/locales',
+  srcPathDirs: [
+    '<rootDir>/src/components',
+    '<rootDir>/src/layouts',
+    '<rootDir>/src/pages',
+    '<rootDir>/src/templates'
+  ],
+  srcPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  format: 'minimal'
+}
