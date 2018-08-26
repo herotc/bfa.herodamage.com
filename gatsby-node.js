a2014dd568002f03e05dc10c73f2399b93bcda07
Quentin Giraud <dev@aethys.io>
2018-08-26 11:36:26 +0200

    fix: change modifyBabelrc to onCreateBabelConfig & modifyWebpackConfig to onCreateWebpackConfig

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,18 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-exports.modifyBabelrc = ({babelrc}) => ({
-  ...babelrc,
-  plugins: babelrc.plugins.concat(
-    ['transform-regenerator'],
-    ['transform-runtime']
-  )
-})
-
-exports.modifyWebpackConfig = ({config, stage}) => {
-  if (stage === 'build-javascript') {
-    // turn off source maps
-    config.merge({devtool: false})
-  }
-}
