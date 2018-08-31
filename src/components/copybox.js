ac9c3ff69a9638f4e7dd9db24273af60359c1f2f
Quentin Giraud <dev@aethys.io>
2018-08-31 22:01:05 +0200

    feat: new CopyBox component

---
+++ src/components/copybox.js
@@ -0,0 +1,52 @@
+// Dependencies
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+// Components
+import { copyToClipboard } from '../browser/copyToClipboard'
+
+import Button from '@material-ui/core/Button'
+
+const Container = styled.div`
+  > h3 {
+    margin-bottom: 0;
+  }
+
+  > p {
+    align-items: stretch;
+    display: flex;
+    margin-top: 0;
+
+    > textarea {
+      background-color: ${({theme}) => theme.palette.primary.light};
+      border: none;
+      color: ${({theme}) => theme.palette.common.white};
+      padding: 8px;
+      resize: vertical;
+      width: 100%;
+    }
+    
+    > button {
+      background-color: ${({theme}) => theme.palette.primary.light};
+      border-radius: 0;
+    }
+  }
+`
+
+const CopyBox = ({elementId, text, title, ...props}) => (
+  <Container {...props}>
+    <h3>{title}</h3>
+    <p>
+      <textarea id={elementId} rows="3" readOnly>{text}</textarea>
+      <Button onClick={() => { copyToClipboard(elementId) }}>Copy</Button>
+    </p>
+  </Container>
+)
+
+CopyBox.propTypes = {
+  elementId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default CopyBox
