461b3e2e13a9d93e18f4a8100447e28a9915ae73
Quentin Giraud <dev@aethys.io>
2018-10-01 03:25:25 +0200

    feat: [Plugins] reports: Implements ReportTransformer

---
+++ plugins/gatsby-plugin-herodamage-reports/transformer/index.js
@@ -0,0 +1,283 @@
+import { promisify } from 'util'
+import { readFile } from 'fs'
+import { resolve } from 'path'
+import {
+  getAzeriteInformationById,
+  getAzeriteInformationByName,
+  getWowClassIdAndSpecId
+} from '../../../src/utils/wow/core'
+import * as mapping from './mapping'
+
+// Until we get promisified version from fs (promises API is still experimental)
+const readFilePromise = promisify(readFile)
+
+export class ReportTransformer {
+  constructor (node) {
+    this.node = node
+    this.nodeFields = {} // Hold all the fields that will be registered to the corresponding page node
+    this.extraFields = {} // Hold all the fields that will not be registered but can be used by the transformer
+
+    const { name } = this.node
+    const [simulationName, fightStyle, tier, wowClass, spec, variation] = name.toLowerCase().split('_')
+    this.extraFields.simulationName = simulationName
+
+    const { simulationFeaturedOrder, simulationCategory, simulationType, simulationTemplate } = mapping.simulationDetails[simulationName]
+    let slug = `/${wowClass}/${simulationType}/${fightStyle}-${tier}-${spec}`
+    if (variation) slug += `-${variation}`
+    Object.assign(this.nodeFields, {
+      slug, // '/death-knight/trinkets/1t-t21-frost-cold-heart-runic-attenuation'
+      name, // 'TrinketSimulation_1T_T21_Death-Knight_Frost_Cold-Heart-Runic-Attenuation'
+      wowClass, // 'death-knight'
+      simulationFeaturedOrder: simulationFeaturedOrder || null, // 2
+      simulationCategory, // 'trinkets'
+      simulationType, // 'trinkets'
+      simulationTemplate, // 'trinkets'
+      tier, // 't21'
+      spec, // 'frost'
+      variation: variation || '', // 'cold-heart-runic-attenuation'
+      fightStyle // '1t'
+    })
+  }
+
+  async extractDataFromFile () {
+    const { absolutePath, name } = this.node
+    const { simulationType } = this.nodeFields
+
+    // Fetch the report file
+    let report
+    try {
+      const jsonFile = await readFilePromise(absolutePath, { encoding: 'utf8', flag: 'r' })
+      report = JSON.parse(jsonFile)
+    } catch (err) {
+      console.error(`Error while processing the '${name}' report:`, err)
+      return
+    }
+    const { metas, results } = report
+
+    this.extraFields.results = results
+
+    let processedResults
+    switch (simulationType) {
+      case 'combinations-default':
+        let label = ''
+        const powerLabels = []
+        for (const simcEncoded of metas.templateGear) {
+          const parts = simcEncoded.split(',')
+          for (const part of parts) {
+            const [option, value] = part.split('=')
+            switch (option) {
+              case 'azerite_powers':
+                const powersId = value.split('/')
+                for (const powerId of powersId) {
+                  if (parseInt(powerId) <= 12) continue
+                  const azeriteInformation = getAzeriteInformationById(powerId)
+                  if (!azeriteInformation) {
+                    console.log(`Cannot find information about azerite powerId "${powerId}" in "${name}"`)
+                    continue
+                  }
+                  const { spellName, tier } = azeriteInformation
+                  if (tier === 3) powerLabels.push(spellName)
+                }
+                break
+            }
+          }
+        }
+        label += powerLabels.join(' / ')
+        processedResults = results.map((result) => {
+          result[3] = label
+          return result
+        })
+        break
+      case 'races':
+        // TODO: Proper handling of Races labels instead of ignoring variations
+        processedResults = results.filter((result) => typeof result[0] !== 'string' || !result[0].includes('--'))
+        break
+      default:
+        processedResults = results
+        break
+    }
+
+    Object.assign(this.nodeFields, {
+      resultsRaw: JSON.stringify(processedResults),
+      fightLength: metas.fightLength,
+      fightLengthVariation: metas.fightLengthVariation,
+      targetError: metas.targetError,
+      templateGear: metas.templateGear,
+      templateTalents: metas.templateTalents,
+      templateDPS: metas.templateDPS,
+      elapsedTime: metas.elapsedTime,
+      totalEventsProcessed: metas.totalEventsProcessed,
+      totalIterations: metas.totalIterations,
+      totalActors: metas.totalActors,
+      simcBuildTimestamp: metas.simcBuildTimestamp,
+      simcGitRevision: metas.simcGitRevision || '',
+      wowVersion: metas.wowVersion,
+      wowBuild: metas.wowBuild
+    })
+  }
+
+  generateAdditionalNodeFields () {
+    const { fightStyle, simulationType, spec, tier, wowClass } = this.nodeFields
+    const { results } = this.extraFields
+
+    // Generate AzeriteForge & AzeritePowerWeights Import String
+    switch (simulationType) {
+      case 'azerite-levels':
+      case 'azerite-stacks':
+        // Build the powers arrays
+        const afPowers = [] // AzeriteForge (AF)
+        const apwPowers = [] // AzeritePowerWeights (APW)
+        for (let i = 1; i < results.length; i++) {
+          const value = results[i]
+          const parts = value.shift().split('--') // Split up the variations, those aren't supported by the addons atm
+          const spellNames = parts[0].split(' / ') // Some labels are concatened, like the Alliance / Horde one, we always take the first one
+          // Discard RA results
+          if (!parts[1] || !parts[1].includes('ra:')) {
+            // Insert each power (powerId and meanDPS)
+            for (const spellName of spellNames) {
+              const { powerId } = getAzeriteInformationByName(spellName)
+
+              // Use actual values for AzeriteForge
+              const afWeights = []
+              for (let j = 1; j < results[0].length; j++) {
+                afWeights.push(`${results[0][j]}:${value[j - 1]}`)
+              }
+              const afString = `[${powerId}]${afWeights.join(',')},^`
+              let existingIdx = afPowers.findIndex(item => item.includes(`[${powerId}]`))
+              if (existingIdx < 0) {
+                afPowers.push(afString)
+              } else if (parts[1] && parts[1].includes('talents:')) {
+                afPowers[existingIdx] = afString
+              }
+
+              // Calculate mean DPS for AzeritePowerWeights
+              const totalDPS = value.reduce((accumulator, currentValue) => accumulator + currentValue)
+              const meanDPS = totalDPS / value.length
+              existingIdx = apwPowers.findIndex(item => item.powerId === powerId)
+              if (existingIdx < 0) {
+                apwPowers.push({ powerId, meanDPS })
+              } else if (parts[1] && parts[1].includes('talents:')) {
+                apwPowers[existingIdx].meanDPS = meanDPS
+              }
+            }
+          }
+        }
+
+        const { classId, specId } = getWowClassIdAndSpecId(wowClass, spec)
+
+        // Create the import string for AzeriteForge
+        const afWeightsString = `AZFORGE:${classId}:${specId}^${afPowers.join('')}`
+
+        // Create the import string for AzeritePowerWeights
+        apwPowers.sort((a, b) => b.meanDPS - a.meanDPS) // Descending sort using meanDPS
+        // Compute the weights
+        const bestPower = apwPowers[0]
+        bestPower.weight = 10 // Defined by the addon as reference
+        for (let i = 1; i < apwPowers.length; i++) {
+          // Compute the weight relatively to the best power
+          const power = apwPowers[i]
+          power.weight = (power.meanDPS / bestPower.meanDPS * bestPower.weight).toFixed(2)
+        }
+        const apwWeights = apwPowers.map(({ powerId, weight }) => `${powerId}=${weight}`)
+        const apwWeightsName = `herodamage.com - ${simulationType === 'azeritelevels' ? 'Levels' : 'Stacks'}_${fightStyle.toUpperCase()}_${tier.toUpperCase()}`
+        const apwWeightsString = `( AzeritePowerWeights:1:"${apwWeightsName}":${classId}:${specId}: ${apwWeights.join(', ')} )`
+
+        // Save them
+        Object.assign(this.nodeFields, {
+          azeriteForgeWeights: afWeightsString,
+          azeritePowerWeights: apwWeightsString
+        })
+        break
+    }
+  }
+
+  registerPagesDetails () {
+    const { wowClasses, simulationsName, fightStyles, tiers, nodeFieldsName } = ReportTransformer.pagesDetails
+    const { wowClass, fightStyle, tier } = this.nodeFields
+    const { simulationName } = this.extraFields
+
+    // Used to create WoW classes & Performance pages
+    if (!wowClasses.includes(wowClass)) wowClasses.push(wowClass)
+    if (!simulationsName.includes(simulationName)) simulationsName.push(simulationName)
+    if (!fightStyles.includes(fightStyle)) fightStyles.push(fightStyle)
+    if (!tiers.includes(tier)) tiers.push(tier)
+
+    // Used to automatically retrieve all nodeFields registered through the transformer in the simulation query
+    for (const nodeFieldName of Object.keys(this.nodeFields)) {
+      if (!nodeFieldsName.includes(nodeFieldName)) nodeFieldsName.push(nodeFieldName)
+    }
+  }
+
+  createNodeFields (actions) {
+    const node = this.node
+    const { createNodeField } = actions
+
+    for (const name in this.nodeFields) {
+      const value = this.nodeFields[name]
+      createNodeField({ node, name, value })
+    }
+  }
+
+  static async createPages (api) {
+    const { graphql, actions: { createPage } } = api
+    const { wowClasses, simulationsName, fightStyles, tiers, nodeFieldsName } = ReportTransformer.pagesDetails
+
+    // WoW classes
+    for (const wowClass of wowClasses) {
+      const slug = `/${wowClass}/`
+      createPage({ path: slug, component: resolve('./src/templates/wow-class.js'), context: { slug, wowClass } })
+    }
+
+    // SimC Performance
+    for (const simulationName of simulationsName) {
+      for (const fightStyle of fightStyles) {
+        for (const tier of tiers) {
+          const { simulationFeaturedOrder, simulationCategory, simulationType } = mapping.simulationDetails[simulationName]
+          const slug = `/simc-performance/${simulationType}/${fightStyle}-${tier}`
+          createPage({
+            path: slug,
+            component: resolve('./src/templates/simc-performance.js'),
+            context: { slug, simulationFeaturedOrder, simulationCategory, simulationType, fightStyle, tier }
+          })
+        }
+      }
+    }
+
+    // Simulations
+    const result = await graphql(`
+      {
+        allFile(filter: {sourceInstanceName: {eq: "reports"}}) {
+          edges {
+            node {
+              fields {
+                ${nodeFieldsName.join(' ')}
+              }
+            }
+          }
+        }
+      }
+    `)
+    const { data: { allFile: { edges } } } = result
+    edges.forEach(({ node }) => {
+      const fields = node.fields
+      const { slug, simulationTemplate } = fields
+      createPage({
+        path: slug,
+        component: resolve(`./src/templates/simulation/${simulationTemplate}.js`),
+        context: fields
+      })
+    })
+  }
+}
+
+/**
+ * Hold all the details to create the pages
+ * @type {{wowClasses: Array, simulationsName: Array, fightStyles: Array, tiers: Array, nodeFieldsName: Array}}
+ */
+ReportTransformer.pagesDetails = {
+  wowClasses: [],
+  simulationsName: [],
+  fightStyles: [],
+  tiers: [],
+  nodeFieldsName: []
+}
---
+++ plugins/gatsby-plugin-herodamage-reports/transformer/mapping.js
@@ -0,0 +1,51 @@
+export const simulationDetails = {
+  'azerite-levels': {
+    simulationFeaturedOrder: 1,
+    simulationCategory: 'azerite',
+    simulationType: 'azerite-levels',
+    simulationTemplate: 'azerite-levels'
+  },
+  'azerite-stacks': {
+    simulationCategory: 'azerite',
+    simulationType: 'azerite-stacks',
+    simulationTemplate: 'azerite-stacks'
+  },
+  'combinator': {
+    simulationCategory: 'combinations',
+    simulationType: 'combinations-default',
+    simulationTemplate: 'combinations'
+  },
+  'combinator-0a': {
+    simulationCategory: 'combinations',
+    simulationType: 'combinations-0a',
+    simulationTemplate: 'combinations'
+  },
+  'combinator-1a': {
+    simulationCategory: 'combinations',
+    simulationType: 'combinations-1a',
+    simulationTemplate: 'combinations'
+  },
+  'combinator-2a': {
+    simulationCategory: 'combinations',
+    simulationType: 'combinations-2a',
+    simulationTemplate: 'combinations'
+  },
+  'combinator-3a': {
+    simulationFeaturedOrder: 3,
+    simulationCategory: 'combinations',
+    simulationType: 'combinations-3a',
+    simulationTemplate: 'combinations'
+  },
+  racesimulation: {
+    simulationFeaturedOrder: 4,
+    simulationCategory: 'races',
+    simulationType: 'races',
+    simulationTemplate: 'races'
+  },
+  trinketsimulation: {
+    simulationFeaturedOrder: 2,
+    simulationCategory: 'trinkets',
+    simulationType: 'trinkets',
+    simulationTemplate: 'trinkets'
+  }
+}
