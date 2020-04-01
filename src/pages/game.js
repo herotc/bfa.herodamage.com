f5823bb779d924e0b00fbe36b0f4c65829be2982
Florian Meißner <developer@mystler.eu>
2020-04-01 17:03:35 +0200

    fix(content): Fix typo and add a ZIP alternative for the game

--- src/pages/game.js
+++ src/pages/game.js
@@ -16,9 +16,11 @@ const GamePage = ({ data }) => (
       <p>You want things to happen? You want to be entertained?</p>
       <p>Then wait no more, for now we offer the perfect companion solution for our site.</p>
       <Button variant="contained" size="large" href="https://mystler.eu/dl/HeroDamageGame_Setup.exe" disableElevation>
-        Download<br/>Hero Damage: The Game
+        Download<br/>Hero Damage: The Game<br/>(Installer)
       </Button>
-      <p><i>(Hero Damage: The Game is currently only availbale for Windows. Sorry!)</i></p>
+      <p><i>Hero Damage: The Game is currently only available for Windows and in English.</i></p>
+      <p><a href="https://mystler.eu/dl/HeroDamageGame.zip">Download Hero Damage: The Game (ZIP)</a><br/>in case you don't want (or your browser doesn't trust) the installer.</p>
+      <p><i>Because of the way the Hero Damage website is generated from simulation data, these game files are instead hosted on Mystler's server. If you have issues with your browser or windows defender blocking the installer, you can get the ZIP file instead and unpack it to a location of your choice.</i></p>
     </section>
   </div>
 )
